2816735826415bdb3981e8f391eb206d7a860d05
UzForex <atabekovshodlik.1@gmail.com>
2025-11-11 16:14:51 +0000

    iltimos kuting qo'shdim

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
   const firstname = msg.chat.first_name;
+  const menuPhoto = "bmw-3.jpg";
 
   if (text == "/start") {
     bot.sendMessage(chatId, `xushkelipsiz, ${firstname}`, {
@@ -14,10 +15,19 @@ bot.on("message", async (msg) => {
       },
     });
   } else if (text == "BMW M3 malumot lar") {
-    bot.sendMessage(
-      chatId,
-      "BMW M3 - bu nemis avtomobil ishlab chiqaruvchisi BMW tomonidan 1986-yildan beri ishlab chiqarilayotgan yuqori samarali sport avtomobili. U turli avlodlarda turli tana shakllarida (sedan, kupe, kabriolet va universal) mavjud. Eng so'nggi avlodlarida (353) kVt ((480) ot kuchi) gacha bo'lgan (M) TwinPower Turbo inline-6 dvigateli va (0) dan (100) km/soat tezlikka (4.2) soniyada erishish mumkin. "
-    );
+    const Kutishtext = await bot.sendMessage(chatId, "iltimos kuting....");
+
+    setTimeout(() => {
+        bot.deleteMessage(chatId, Kutishtext.message_id)
+        bot.sendPhoto(chatId, menuPhoto, {
+          caption:
+            "BMW M3 - bu nemis avtomobil ishlab chiqaruvchisi BMW tomonidan 1986-yildan beri ishlab chiqarilayotgan yuqori samarali sport avtomobili. U turli avlodlarda turli tana shakllarida (sedan, kupe, kabriolet va universal) mavjud. Eng so'nggi avlodlarida (353) kVt ((480) ot kuchi) gacha bo'lgan (M) TwinPower Turbo inline-6 dvigateli va (0) dan (100) km/soat tezlikka (4.2) soniyada erishish mumkin. ",
+        
+    
+        
+        });
+    }, 2000);
+    
   }
 });
 console.log("ISHGA TUSHDI");
