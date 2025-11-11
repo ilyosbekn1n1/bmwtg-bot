896ec76532c9fe4229e54faf980486beaef510fb
UzForex <atabekovshodlik.1@gmail.com>
2025-11-11 15:35:11 +0000

    bo't ishga tushdi

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import TelegramBot from "node-telegram-bot-api";
-const TOKEN = "8568037431:AAEMllBtiNJtSt1vok-OsCVLrjh8pNo3DZA"
-const bot = new TelegramBot(TOKEN, {polling: true});
+const TOKEN = "8568037431:AAEMllBtiNJtSt1vok-OsCVLrjh8pNo3DZA";
+const bot = new TelegramBot(TOKEN, { polling: true });
 
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstname = msg.chat.first_name;
 
+if (text == "/start") {
+    bot.sendMessage(chatId, `xushkelipsiz, ${firstname}`)
+    
+}
 
+});
+console.log("ISHGA TUSHDI");
